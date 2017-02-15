6914140c5b184f26fcbbe801e48a584678e7ff35
Batakrishna Sahu <batakrishna.sahu@suiit.ac.in>
2017-02-15 16:01:40 +0530

    src add

---
+++ src/hello-world/hello_world.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Batakrishna Sahu on 13/2/17.
+ * A simple hello-world program that will give an output "Hello World !"
+ */
+
+ public class hello_world {
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+	}
+ }
