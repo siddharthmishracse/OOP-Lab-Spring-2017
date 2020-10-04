21269f099ac407e577a10ab60a7a1ba53c4e455e
Venkat Prasad Kalet <venkatprasadkalet@gmail.com>
2020-10-04 09:50:24 +0530

    Update hello_world.java

--- src/1. hello-world/hello_world.java
+++ src/1. hello-world/hello_world.java
@@ -5,6 +5,6 @@
 
  public class hello_world {
 	public static void main(String[] args) {
-		System.out.println("Hello Baby,don't do anything because I am only doing it for hectoberfest. !");
+		System.out.println("Hello Baby,don't do anything because I am only doing it for hectoberfests. !");
 	}
  }
